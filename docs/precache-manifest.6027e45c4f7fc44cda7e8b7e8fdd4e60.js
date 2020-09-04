00c3abf4a3b3f7abf4fb97e787f6625c993d8f13
Agus <silveraguskn@gmail.com>
2020-09-03 22:33:24 -0300

    Docs agregado.

---
+++ docs/precache-manifest.6027e45c4f7fc44cda7e8b7e8fdd4e60.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e3375ac89de1387a849a07404ad489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32e87d1ac348626c92b1",
+    "url": "/static/css/main.a91d0bb4.chunk.css"
+  },
+  {
+    "revision": "2cbfe04becb45bdd55a7",
+    "url": "/static/js/2.4ac104d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4ac104d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32e87d1ac348626c92b1",
+    "url": "/static/js/main.823c3227.chunk.js"
+  },
+  {
+    "revision": "9b41c7e13f690904baac",
+    "url": "/static/js/runtime-main.c3e8af0c.js"
+  },
+  {
+    "revision": "57027a92e6f88dae8ecca4af739cc8ae",
+    "url": "/static/media/Avatar.57027a92.jpg"
+  },
+  {
+    "revision": "d05d0c7b3c4995580cc588dadae87092",
+    "url": "/static/media/chat-pattern.d05d0c7b.svg"
+  },
+  {
+    "revision": "1e836ab53b5d041a8f738e382750c63c",
+    "url": "/static/media/phone.1e836ab5.svg"
+  }
+]);
